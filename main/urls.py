35f8863dc5789e7cd83e34e3b700263f75979b45
rituparnadobey-dci <dobeyrituparna@gmail.com>
2024-06-27 20:05:37 +0200

    reg5

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('profile_list/', views.profile_list, name='profile_list'),
     path('profile/<int:pk>/', views.profile, name='profile'),  # Added trailing slash
     path('profile/<int:pk>/follow_unfollow/', views.follow_unfollow, name='follow_unfollow'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
